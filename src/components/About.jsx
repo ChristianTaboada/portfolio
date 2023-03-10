0d91bab104f9e9fb5f6c77213963ac3bd562d7f9
ChristianTaboada <taboada.christian@gmail.com>
2023-03-10 02:58:31 -0300

    about terminado

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import CountUp from 'react-countup'
+import{useInView} from 'react-intersection-observer'
+import {motion} from 'framer-motion'
+import {fadeIn} from '../variants'
 
 const About = () =>{
+    const [ref, inView] = useInView({
+        threshold: 0.5,
+    })
     return(
-        <div className='section' id='about'>About</div>
+        <section className='section' id='about' ref={ref}>
+            <div className="container mx-auto">
+                <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+                    {/*imagen*/}
+                    <motion.div
+                    variants={fadeIn('right', 0.3)}
+                    initial='hidden'
+                    whileInView={'show'}
+                    viewport={{ once: false, amount: 0.3 }}
+                    className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+                    </motion.div>
+                    {/*text*/}
+                    <motion.div
+                    variants={fadeIn('left', 0.5)}
+                    initial='hidden'
+                    whileInView={'show'}
+                    viewport={{ once: false, amount: 0.3 }} 
+                    className='flex-1'>
+                        <h2 className='h2 text-accent'>About me</h2>
+                        <h3 className='h3 mb-4'>
+                            I'm a Developer Full Stack with over 6 month of experience,
+                        </h3>
+                        <p className='mb-6'>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum deleniti enim illo id sapiente expedita soluta voluptatum distinctio dolores reiciendis? Eum amet officia numquam iste quod, mollitia magni adipisci reiciendis.
+                        </p>
+                        {/*stats*/}
+                        <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+                            <div>
+                                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                                    {
+                                    inView? <CountUp start={0} end={6} duration={3}/> : null
+                                    }
+                                </div>
+                                <div className='font-primary text-sm tracking-[2px]'>Mounth of <br/>
+                                Experience
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button className='btn btn-lg'>Contact me</button>
+                            <a href='#' className='text-gradient btn-link'>
+                                My Portfolio
+                            </a>
+                        </div>
+                    </motion.div>
+                </div>
+            </div>
+        </section>
     )
 };
 
